5d683ae07422d24c9c5ea2892a63cb76d367327e
chance <chance_st@yahoo.com>
2020-07-12 14:34:40 -0700

    basic parsing for lxrs

--- Fantasy/2020/beta/code/python/luxurias2019.py
+++ Fantasy/2020/beta/code/python/luxurias2019.py
@@ -14,7 +14,7 @@ out_date = now.strftime("%m%d%Y-%H%M%S")
 
 #print(type(data))
 
-with urllib.request.urlopen("http://fantasy.espn.com/apis/v3/games/flb/seasons/2019/segments/0/leagues/162788?view=roster") as url:
+with urllib.request.urlopen("http://fantasy.espn.com/apis/v3/games/flb/seasons/2020/segments/0/leagues/162788?view=roster") as url:
     data = json.loads(url.read().decode())
 
 #print(type(data))
@@ -28,29 +28,34 @@ with urllib.request.urlopen("http://fantasy.espn.com/apis/v3/games/flb/seasons/2
 #print( "Team: " + team['nickname'])
 
 for team in data['teams']:
-    team_name = team['location'] + " " + team['nickname']
-    for player in team['roster']['entries']:
-        player_full_name = player['playerPoolEntry']['player']['fullName'] 
-        #print( player['playerPoolEntry']['player']['fullName'] )
-        #print( player['playerPoolEntry'].keys() )
-        for pool_entry_key in player['playerPoolEntry'].keys():
-            #print( pool_entry_key )
-            if pool_entry_key != "player":
-                pass
-                #print( player['playerPoolEntry'][pool_entry_key])
-            else:
-                #print( player['playerPoolEntry'][pool_entry_key].keys())
-                for player_key in player['playerPoolEntry']['player'].keys():
-                    #print(player_key)
-                    if player_key != "ownershipHistory":
-                        pass
-                        #print(player['playerPoolEntry']['player'][player_key])
-                    else:
-                        for item in player['playerPoolEntry']['player'][player_key]:
-                            item_date = time.localtime(item['date'] / 1000)
-                            history_date = time.strftime('%Y%m%d', item_date)
-                            print(team_name + " , " + player_full_name + " , " + history_date + " , " + str(item['percentOwned']))
-                    
+    if team['id'] == 4:
+        team_name = team['location'] + " " + team['nickname'] + " (" + str(team['id']) + ")"
+        print(team_name)
+        for player in team['roster']['entries']:
+            player_full_name = player['playerPoolEntry']['player']['fullName'] 
+            #print( player['playerPoolEntry']['player']['fullName'] )
+            #print( player['playerPoolEntry'].keys() )
+            for pool_entry_key in player['playerPoolEntry'].keys():
+                #print( pool_entry_key )
+                if pool_entry_key != "player":
+                    pass
+                    #print( player['playerPoolEntry'][pool_entry_key])
+                else:
+                    #print( player['playerPoolEntry'][pool_entry_key].keys())
+                    for player_key in player['playerPoolEntry']['player'].keys():
+                        #print(player_key)
+                        if player_key != "ownershipHistory":
+                            pass
+                            #print(player['playerPoolEntry']['player'][player_key])
+                        else:
+                            for item in player['playerPoolEntry']['player'][player_key]:
+                                item_date = time.localtime(item['date'] / 1000)
+                                history_date = time.strftime('%Y%m%d', item_date)
+                                percent_owned = str(round(item['percentOwned'],2))
+                                auction_value = str(round(item['auctionValueAverage'],2))
+                                print(team_name + " , " + player_full_name + " , " + history_date + " , " +
+                                      percent_owned  + " , " + auction_value)
+                        
                 
     #print("\n\n")
     
