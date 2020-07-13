ebed473d171aa40c2736d4e7f5504187a7b45422
chance <chance_st@yahoo.com>
2020-07-12 22:56:39 -0700

    roster changes added

--- Fantasy/2020/beta/code/python/roster_changes.py
+++ Fantasy/2020/beta/code/python/roster_changes.py
@@ -4,66 +4,88 @@ import time
 from datetime import datetime
 import sys
 sys.path.append('./modules')
-print(sys.path)
 import sqldb
 
+def get_teams():
+
+    c = bdb.select("SELECT LeagueID, Name FROM Leagues where Active = 'True'")
+    for t in c:
+        league_dict[t[0]] = t[1]
+
+    c = bdb.select("SELECT * FROM Rosters where LeagueID  in (SELECT LeagueID FROM Leagues where Active = 'True')")
+    for t in c:
+        old_rosters[t[0] + ':' + t[1]] = t[1]
+        team_dict[t[1]] = t[2]
+
+def print_teams():
+    for league in league_dict:
+        print( str(league) + ", " + league_dict[league])
+
+def print_rosters():
+    for key in old_rosters:
+        print( key + ", " + old_rosters[key])
 
 
-#print("Load start ")
+def get_new_rosters():
+    new_rosters = {}
+
+    for league in league_dict:
+        addr = "http://fantasy.espn.com/apis/v3/games/flb/seasons/2020/segments/0/leagues/" + str(league) + "?view=roster"
+        print(addr)
+
+        with urllib.request.urlopen(addr) as url:
+            data = json.loads(url.read().decode())
+
+        for team in data['teams']:
+            team_name = team['location'] + " " + team['nickname']
+            for player in team['roster']['entries']:
+                player_full_name = player['playerPoolEntry']['player']['fullName']
+                command = "INSERT INTO Rosters(Player, Team, LeagueID) VALUES ( \"" + player_full_name + \
+                          "\" ,\"" \
+                          + \
+                          team_name + "\"," + str(league) + ")"
+                new_rosters[player_full_name + ':' + team_name] = team_name
+                print(command)
+                insert_list.append(command)
+    return
+
+
+def update_rosters():
+    players = len(insert_list)
+    msg = ""
+    minimum = 200
+
+    if (players < minimum):
+        print("Not enough players in new roster list: " + str(players))
+        msg += "Not enough players in new roster list"
+        inst.push("Roster error: " + str(date_time), msg)
+        time.sleep(2)
+        inst.push("Roster error: " + str(date_time), msg)
+    else:
+        print("New roster list appears to be full, number of players: " + str(players))
+        print("Updating Rosters table")
+        c = bdb.delete("DELETE FROM Rosters where LeagueID  in (SELECT LeagueID FROM Leagues where Active = 'True')")
+        for command in insert_list:
+            print(command)
+            bdb.insert(command)
+
+
+import push
+inst = push.Push()
+
+bdb = sqldb.DB('Baseball.db')
+
 now = datetime.now() # current date and time
 date_time = now.strftime("%m/%d/%Y-%H:%M:%S")
 out_date = now.strftime("%m%d%Y-%H%M%S")
-#print(now)
-#print(date_time)
 
-#with urllib.request.urlopen("http://fantasy.espn.com/apis/v3/games/flb/seasons/2020/segments/0/leagues/37863846?view=roster") as url:
-  #  data = json.loads(url.read().decode())
+league_dict = {}
+team_dict = {}
+old_rosters = {}
+insert_list = []
 
-#print(type(data))
-
-with urllib.request.urlopen("http://fantasy.espn.com/apis/v3/games/flb/seasons/2020/segments/0/leagues/37863846?view=roster") as url:
-    data = json.loads(url.read().decode())
-
-#print(type(data))
-
-#print(data.keys())
-
-#print(  data['teams'][0]['roster']['entries'][0]['playerPoolEntry']['player']['fullName'])
-
-#for team in data['teams']:
-#team = data['teams'][5]
-#print( "Team: " + team['nickname'])
-
-for team in data['teams']:
-    team_name = team['location'] + " " + team['nickname']
-    for player in team['roster']['entries']:
-        player_full_name = player['playerPoolEntry']['player']['fullName'] 
-        #print( player['playerPoolEntry']['player']['fullName'] )
-        #print( player['playerPoolEntry'].keys() )
-        for pool_entry_key in player['playerPoolEntry'].keys():
-            #print( pool_entry_key )
-            if pool_entry_key != "player":
-                pass
-                #print( player['playerPoolEntry'][pool_entry_key])
-            else:
-                #print( player['playerPoolEntry'][pool_entry_key].keys())
-                for player_key in player['playerPoolEntry']['player'].keys():
-                    #print(player_key)
-                    if player_key != "ownershipHistory":
-                        pass
-                        #print(player['playerPoolEntry']['player'][player_key])
-                    else:
-                        for item in player['playerPoolEntry']['player'][player_key]:
-                            item_date = time.localtime(item['date'] / 1000)
-                            history_date = time.strftime('%Y%m%d', item_date)
-                            print(team_name + " , " + player_full_name + " , " + history_date + " , " + str(item['percentOwned']))
-                    
-                
-    #print("\n\n")
-    
-#print("Done loading")
-now = datetime.now() # current date and time
-date_time = now.strftime("%m/%d/%Y-%H:%M:%S")
-out_date = now.strftime("%m%d%Y-%H%M%S")
-#print(time.time())
-#print(date_time)
+get_teams()
+print_teams()
+print_rosters()
+get_new_rosters()
+update_rosters()
