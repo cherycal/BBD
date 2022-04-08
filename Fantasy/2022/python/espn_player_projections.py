b5f1567305041e20919870c14f5fbf209c1635fe
cherycal <chance_st@yahoo.com>
2022-04-08 13:34:35 -0700

    Add files via upload

---
+++ Fantasy/2022/python/espn_player_projections.py
@@ -0,0 +1,144 @@
+import json
+import sys
+import time
+sys.path.append('./modules')
+import sqldb
+import pycurl
+import certifi
+from io import BytesIO
+import pandas as pd
+
+
+def main():
+	bdb = sqldb.DB('Baseball.db')
+	yr = 2022
+
+	dfheaders = ["Name", "id", "Year", "AuctionValue", "rank",
+	             "AuctionValueAvg", "AuctionChange", "AB", "H", "AVG", "2B", "3B", "HR", "BB", "K",
+	             "SLG", "OBP", "OPS", "R", "RBI", "SB", "GIDP",
+	             "IPOUTS", "H_OPP", "HR_OPP", "ER", "K_OPP", "BB_OPP", "W", "L",
+	             "WHIP", "ERA", "SV", "HD", "QS","RA","BS","BAA"]
+
+	dashes = ['NULL', yr, yr]
+	for i in range(35):
+		dashes.append(0)
+	print(dashes)
+
+	df = pd.DataFrame([dashes], columns=dfheaders)
+
+	url_name = "https://fantasy.espn.com/apis/v3/games/flb/" \
+	           "seasons/" + str(yr) + "/segments/0/leaguedefaults/1?view=kona_player_info"
+
+	headers = ['authority: fantasy.espn.com',
+	           'accept: application/json',
+	           'x-fantasy-source: kona',
+	           'x-fantasy-filter: {"players":{"filterStatus":{"value":["FREEAGENT","WAIVERS","ONTEAM"]},'
+	           '"filterSlotIds":{"value":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}}}']
+
+	print("get_player_data_json: " + url_name)
+	buffer = BytesIO()
+	c = pycurl.Curl()
+	c.setopt(c.URL, url_name)
+	c.setopt(c.HTTPHEADER, headers)
+	c.setopt(c.WRITEDATA, buffer)
+	c.setopt(c.CAINFO, certifi.where())
+	c.perform()
+	c.close()
+
+	data = buffer.getvalue()
+
+	csv_file = str(yr) + "proj.csv"
+	# f = open(str(yr) + "proj.txt", "w")
+	json_data = json.loads(data)
+	# f.write(json.dumps(json_data))
+	# f.close()
+
+	for player in json_data['players']:
+		pl = []
+		# print(player['fullName'])
+		pi = player['player']
+		pl.append(pi['fullName'])
+		# print("id: " + str(pi['id']))
+		pl.append(pi['id'])
+		pl.append(yr)
+		try:
+			# print("auction value: " + str(pi['draftRanksByRankType']['STANDARD']['auctionValue']))
+			pl.append(pi['draftRanksByRankType']['STANDARD']['auctionValue'])
+			pl.append(pi['draftRanksByRankType']['STANDARD']['rank'])
+		except KeyError:
+			pl.append(0)
+			pl.append(0)
+		try:
+			pl.append(abs(pi['ownership']['auctionValueAverage']))
+			pl.append(abs(pi['ownership']['auctionValueAverageChange']))
+		except KeyError:
+			pl.append(0)
+			pl.append(0)
+		proj = 0
+		if pi.get('stats'):
+			#print(pi['fullName'])
+			for stat in pi['stats']:
+				# 00: actual 10: projected
+				if stat['id'] == '10' + str(yr):
+					# if pi['lastName'] == "Staumont":
+					# 	time.sleep(5)
+					# 	pass
+					proj = 1
+					stats = stat['stats']
+					# print(stats)
+					pl.append(stats.get('0', 0))
+					pl.append(stats.get('1', 0))
+					pl.append(stats.get('2', 0))
+					pl.append(stats.get('3', 0))
+					pl.append(stats.get('4', 0))
+					pl.append(stats.get('5', 0))
+					pl.append(stats.get('10', 0))
+					pl.append(stats.get('27', 0))
+					pl.append(stats.get('9', 0))
+					pl.append(stats.get('17', 0))
+					pl.append(stats.get('18', 0))
+					pl.append(stats.get('20', 0))
+					pl.append(stats.get('21', 0))
+					pl.append(stats.get('23', 0))
+					pl.append(stats.get('26', 0))
+					pl.append(stats.get('34', 0))
+					pl.append(stats.get('37', 0))
+					pl.append(stats.get('46', 0))
+					pl.append(stats.get('45', 0))
+					pl.append(stats.get('48', 0))
+					pl.append(stats.get('39', 0))
+					pl.append(stats.get('53', 0))
+					pl.append(stats.get('54', 0))
+					pl.append(stats.get('41', 0))
+					pl.append(stats.get('47', 0))
+					pl.append(stats.get('57', 0))
+					pl.append(stats.get('60', 0))
+					pl.append(stats.get('63', 0))
+					pl.append(stats.get('44', 0))
+					pl.append(stats.get('58', 0))
+					pl.append(stats.get('38', 0))
+			if not proj:
+				pl.extend([0.0] * 31)
+		else:
+			pl.extend([0.0] * 31)
+
+		df = df.append(pd.DataFrame([pl], columns=df.columns))
+
+	df.to_csv(csv_file)
+
+	table_name = "ESPNProjections"
+
+	del_cmd = "delete from " + table_name + " where Year = " + str(yr)
+	bdb.delete( del_cmd)
+
+	df.to_sql(table_name, bdb.conn, if_exists='append', index=False)
+
+	bdb.update("update ESPNProjections set PPTS = IPOUTS-H_OPP-BB_OPP+K_OPP-2*ER+W-L+5*SV+3*HD+2*QS")
+	bdb.update("update ESPNProjections set BPTS = H+'2B'+2*'3B'+3*HR+R+SB+RBI+BB-K")
+
+	bdb.close()
+	exit(0)
+
+
+if __name__ == "__main__":
+	main()
---
+++ Fantasy/2022/python/statcast_daily_level.py
@@ -0,0 +1,167 @@
+__author__ = 'chance'
+
+import sys
+
+sys.path.append('./modules')
+import pandas as pd
+import time
+import push
+from datetime import date, datetime
+from datetime import timedelta
+
+inst = push.Push()
+
+import sqldb
+
+bdb = sqldb.DB('Baseball.db')
+
+
+def do_range(start_date, end_date, player_type, spring_training):
+
+    year = start_date[6:10]
+    start_date8 = year + start_date[0:2] + start_date[3:5]
+    end_date8 = year + end_date[0:2] + end_date[3:5]
+    bpdir = ""
+    if player_type == "batter":
+        bpdir = "Batting"
+    elif player_type == "pitcher":
+        bpdir = "Pitching"
+    else:
+        print("player_type is batter or pitcher")
+        exit(-1)
+
+    st_flag = ""
+    if spring_training:
+        st_flag = "S"
+    else:
+        st_flag = "R"
+
+
+    csvfile = "C:\\Users\\chery\\Documents\\BBD\\Statcast\\" + bpdir + \
+              "\\statcast_daily_" + start_date + "_" + end_date + ".csv"  # "statcast_daily_batting_20210401.csv"
+
+    url_text = "https://baseballsavant.mlb.com/statcast_search?" \
+               "hfPT=&hfAB=single%7Cdouble%7Ctriple%7Chome%5C.%5C.run%7Cfield%5C.%5C.out%" \
+               "7Cstrikeout%7Cstrikeout%5C.%5C.double%5C.%5C.play%7Cwalk%7Cdouble%5C.%5C.play%" \
+               "7Cfield%5C.%5C.error%7Cgrounded%5C.%5C.into%5C.%5C.double%5C.%5C.play%7C" \
+               "fielders%5C.%5C.choice%7Cfielders%5C.%5C.choice%5C.%5C.out%7C" \
+               "force%5C.%5C.out%7Chit%5C.%5C.by%5C.%5C.pitch%7Cintent%5C.%5C.walk%7C" \
+               "sac%5C.%5C.bunt%7Csac%5C.%5C.bunt%5C.%5C.double%5C.%5C.play%7C" \
+               "sac%5C.%5C.fly%7Csac%5C.%5C.fly%5C.%5C.double%5C.%5C.play%" \
+               "7Ctriple%5C.%5C.play%7C" + "&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=" + st_flag + "%7C" + \
+               "&hfC=&hfSea=" + str(year) + "%7C&hfSit=&player_type=" + player_type + "&hfOuts=&opponent=&" \
+                "pitcher_throws=&batter_stands=&hfSA=&game_date_gt=" + start_date + \
+               "&game_date_lt=" + end_date + \
+               "&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&" \
+               "min_results=0&group_by=name-date&sort_col=xwoba&player_event_sort=api_h_launch_speed&" \
+               "sort_order=desc&min_pas=0" \
+               "&chk_stats_date=on" \
+               "&chk_stats_player_id=on" \
+               "&chk_stats_game_pk=on" \
+               "&chk_stats_pa=on" \
+               "&chk_stats_abs=on" \
+               "&chk_stats_bip=on" \
+               "&chk_stats_hits=on" \
+               "&chk_stats_singles=on" \
+               "&chk_stats_dbls=on" \
+               "&chk_stats_triples=on" \
+               "&chk_stats_hrs=on" \
+               "&chk_stats_so=on" \
+               "&chk_stats_k_percent=on" \
+               "&chk_stats_bb=on" \
+               "&chk_stats_bb_percent=on" \
+               "&chk_stats_babip=on" \
+               "&chk_stats_iso=on" \
+               "&chk_stats_ba=on" \
+               "&chk_stats_xba=on" \
+               "&chk_stats_xbadiff=on" \
+               "&chk_stats_slg=on" \
+               "&chk_stats_xslg=on" \
+               "&chk_stats_xslgdiff=on" \
+               "&chk_stats_obp=on" \
+               "&chk_stats_xobp=on" \
+               "&chk_stats_woba=on" \
+               "&chk_stats_xwoba=on" \
+               "&chk_stats_wobadiff=on" \
+               "&chk_stats_velocity=on" \
+               "&chk_stats_launch_speed=on" \
+               "&chk_stats_launch_angle=on" \
+               "&chk_stats_bbdist=on" \
+               "&chk_stats_spin_rate=on" \
+               "&chk_stats_plate_x=on" \
+               "&chk_stats_plate_z=on" \
+               "&chk_stats_release_pos_x=on" \
+               "&chk_stats_release_pos_z=on" \
+               "&chk_stats_pos3_int_start_distance=on" \
+               "&chk_stats_pos4_int_start_distance=on" \
+               "&chk_stats_pos6_int_start_distance=on" \
+               "&chk_stats_pos5_int_start_distance=on" \
+               "&chk_stats_pos7_int_start_distance=on" \
+               "&chk_stats_pos8_int_start_distance=on" \
+               "&chk_stats_pos9_int_start_distance=on" \
+               "&chk_stats_release_extension=on" \
+               "#results"
+
+    # url_text = "https://baseballsavant.mlb.com/statcast_search?hfPT=&hfAB=single%7Cdouble%7Ctriple%7Chome%5C.%5C.run%7Cfield%5C.%5C.out%7Cstrikeout%7Cstrikeout%5C.%5C.double%5C.%5C.play%7Cwalk%7Cdouble%5C.%5C.play%7Cfield%5C.%5C.error%7Cgrounded%5C.%5C.into%5C.%5C.double%5C.%5C.play%7Cfielders%5C.%5C.choice%7Cfielders%5C.%5C.choice%5C.%5C.out%7Cforce%5C.%5C.out%7Chit%5C.%5C.by%5C.%5C.pitch%7Cintent%5C.%5C.walk%7Csac%5C.%5C.bunt%7Csac%5C.%5C.bunt%5C.%5C.double%5C.%5C.play%7Csac%5C.%5C.fly%7Csac%5C.%5C.fly%5C.%5C.double%5C.%5C.play%7Ctriple%5C.%5C.play%7C&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea=2021%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt="+\
+    #            "2021-04-01"+\
+    #            "&game_date_lt=" +\
+    #            "2021-04-02"+\
+    #            "&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name-date&sort_col=xwoba&player_event_sort=api_h_launch_speed&sort_order=desc&min_pas=0&chk_stats_date=on&chk_stats_player_id=on&chk_stats_pa=on&chk_stats_abs=on&chk_stats_bip=on&chk_stats_hits=on&chk_stats_singles=on&chk_stats_dbls=on&chk_stats_triples=on&chk_stats_hrs=on&chk_stats_so=on&chk_stats_k_percent=on&chk_stats_bb=on&chk_stats_bb_percent=on&chk_stats_babip=on&chk_stats_iso=on&chk_stats_ba=on&chk_stats_xba=on&chk_stats_xbadiff=on&chk_stats_slg=on&chk_stats_xslg=on&chk_stats_xslgdiff=on&chk_stats_obp=on&chk_stats_xobp=on&chk_stats_woba=on&chk_stats_xwoba=on&chk_stats_wobadiff=on&chk_stats_velocity=on&chk_stats_launch_speed=on&chk_stats_launch_angle=on&chk_stats_bbdist=on&chk_stats_spin_rate=on&chk_stats_plate_x=on&chk_stats_plate_z=on&chk_stats_release_pos_x=on&chk_stats_release_pos_z=on&chk_stats_pos3_int_start_distance=on&chk_stats_pos4_int_start_distance=on&chk_stats_pos6_int_start_distance=on&chk_stats_pos5_int_start_distance=on&chk_stats_pos7_int_start_distance=on&chk_stats_pos8_int_start_distance=on&chk_stats_pos9_int_start_distance=on&chk_stats_release_extension=on#results"
+
+    print(url_text)
+
+    df = pd.read_html(url_text)[0]
+
+    print("sleeping ....")
+    time.sleep(1)
+
+    df.drop(df.columns[[1, 7, -1]], axis=1, inplace=True)
+    df.columns.values[6] = "id"
+    df.columns.values[7] = "gamepk"
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.strftime('%Y%m%d')
+    df['Season'] = pd.to_numeric(year)
+    df.insert(loc=2, column='playerid', value=df['id'])
+    df.drop(['id'], axis=1, inplace=True)
+
+    df[['LastName', 'FirstName']] = df['Player'].str.split(',', expand=True)
+    df['Player'] = df[['FirstName', 'LastName']].agg(' '.join, axis=1)
+    df['Player'] = df['Player'].str.strip()
+    df.drop(['LastName'], axis=1, inplace=True)
+    df.drop(['FirstName'], axis=1, inplace=True)
+    df['GameType'] = st_flag
+
+    df.to_csv(csvfile, index=False, encoding='utf-8-sig')
+    table_name = "Statcast" + bpdir + "Daily"
+
+    # CONVERT Date to Date8
+    del_cmd = 'DELETE from ' + table_name + ' WHERE Date >= ' + start_date8 + " AND Date <= " + end_date8
+    bdb.delete(del_cmd)
+    df.to_sql(table_name, bdb.conn, if_exists='append', index=False)
+
+    print("done")
+
+
+def main():
+
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    date10 = yesterday.strftime("%m-%d-%Y")
+
+    start_date = date10
+    end_date = date10
+
+    # start_date = "03-30-2021"
+    # end_date = "04-30-2021"
+
+    # player_type = "pitcher"  # "batter or pitcher
+    spring_training = False
+
+    #######################
+
+    do_range(start_date, end_date, "pitcher", spring_training)
+    do_range(start_date, end_date, "batter", spring_training)
+
+
+if __name__ == "__main__":
+    main()
