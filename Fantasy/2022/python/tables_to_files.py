f942aeeadba163e6a2a52c0b2fa250adc1bca5d5
Chance <chance_st@yahoo.com>
2022-07-15 11:56:02 -0700

    initial commit. crates csv files from tables - files used for Tableau
    Signed-off-by: Chance <chance_st@yahoo.com>

---
+++ Fantasy/2022/python/tables_to_files.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append('./modules')
+import sqldb
+
+#plat = tools.get_platform()
+#print(plat)
+#push_instance = push.Push()
+mode = "PROD"
+
+if mode == "TEST":
+    bdb = sqldb.DB('BaseballTest.db')
+else:
+    bdb = sqldb.DB('Baseball.db')
+
+tables = ['ACheck_ID','ACheck_Statcast','AllSplits',
+          'UpcomingStartsWithStats','SituationalEventsRosters','AddDrops']
+
+for tbl in tables:
+    try:
+        bdb.table_to_csv(tbl)
+    except Exception as ex:
+        print(f'Error in table_to_csv {tbl}: {ex}')
