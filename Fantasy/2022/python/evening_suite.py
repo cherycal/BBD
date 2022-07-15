ac03775017c1f2600213baeb2679b2cfc84565fe
Chance <chance_st@yahoo.com>
2022-07-15 11:45:16 -0700

    put scripts in list, added try logic
    Signed-off-by: Chance <chance_st@yahoo.com>

--- Fantasy/2022/python/evening_suite.py
+++ Fantasy/2022/python/evening_suite.py
@@ -1,11 +1,27 @@
 import sys
-
 sys.path.append('./modules')
-import time
+import push
 
+inst = push.Push()
 
-exec(open('odds.py').read())
-time.sleep(4)
-exec(open('espn_daily_scoring_to_db.py').read())
-time.sleep(4)
-exec(open('savant_boxscores.py').read())
+scripts = ['odds.py','espn_daily_scoring_to_db.py',
+           'savant_boxscores.py','fangraphs_universe.py']
+
+for script in scripts:
+    print(f'Trying script: {script}')
+    try:
+        exec(open(script).read())
+        print(f'Script {script} succeeded')
+    except Exception as ex:
+        print(f'Script {script} failed with error {ex}')
+        inst.push("Morning suite process error:", f'Error: {ex}\nFunction: {script}')
+
+#
+#
+# exec(open('odds.py').read())
+# time.sleep(4)
+# exec(open('espn_daily_scoring_to_db.py').read())
+# time.sleep(4)
+# exec(open('savant_boxscores.py').read())
+# time.sleep(4)
+# exec(open('fangraphs_universe.py').read())
