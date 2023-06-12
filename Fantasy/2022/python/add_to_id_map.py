9186e03901fd0505d91b3db152129e49c758a19a
Chance <chance_st@yahoo.com>
2023-06-12 14:21:00 -0700

    Added procedures to fix mapping table under certain conditions
    Signed-off-by: Chance <chance_st@yahoo.com>

--- Fantasy/2022/python/add_to_id_map.py
+++ Fantasy/2022/python/add_to_id_map.py
@@ -9,44 +9,49 @@ inst = push.Push()
 bdb = sqldb.DB('Baseball.db')
 fantasy = fantasy.Fantasy()
 
-idmapids = list()
-idmap_obj = bdb.select_plus("select * from IDMap")
-for d in idmap_obj['dicts']:
-    idmapids.append(d['IDPLAYER'])
 
-#print(f"id map cols: {idmap_obj['column_names']}")
-# numcols = len(idmapcols)
-# nulls = ['NULL'] * numcols
+def run_id_map_fixes():
+    idmapids = list()
+    idmap_obj = bdb.select_plus("select * from IDMap")
+    for d in idmap_obj['dicts']:
+        idmapids.append(d['IDPLAYER'])
 
-missing_obj = bdb.select_plus("select * from ACheck_IDMap_Found")
-#print(missing_obj['dicts'])
-for d in missing_obj['dicts']:
-    #print(d['mlbid'])
-    cmd = ""
-    if d['mlbid'] in idmapids:
-        #print(f"id {d['mlbid']} found")
-        if d['otheridtype'] == "FG":
-            idmapidtype = "IDFANGRAPHS"
-            idnametype = "FANGRAPHSNAME"
-        elif d['otheridtype'] == "ESPN":
-            idmapidtype = "ESPNID"
-            idnametype = 'ESPNNAME'
+    missing_obj = bdb.select_plus("select * from ACheck_IDMap_Found")
+    for d in missing_obj['dicts']:
+        idmapidtype = ""
+        idnametype = ""
+        if d['mlbid'] in idmapids:
+            # print(f"id {d['mlbid']} found")
+            if d['otheridtype'] == "FG":
+                idmapidtype = "IDFANGRAPHS"
+                idnametype = "FANGRAPHSNAME"
+            elif d['otheridtype'] == "ESPN":
+                idmapidtype = "ESPNID"
+                idnametype = 'ESPNNAME'
+            else:
+                pass
+            cmd = f"UPDATE IDMap set TEAM = '{d['mlbTeam']}', POS = '{d['position']}', " \
+                  f"{idmapidtype} = '{d['id']}', {idnametype} = '{d['name']}', " \
+                  f"BATS = '{d['bats']}', THROWS = '{d['throws']}' WHERE IDPLAYER = {d['mlbid']}"
+            print(f"{cmd}")
         else:
-            pass
-        cmd = f"UPDATE IDMap set TEAM = '{d['mlbTeam']}', POS = '{d['position']}', {idmapidtype} = {d['id']}, {idnametype} = '{d['name']}', " \
-              f"BATS = '{d['bats']}', THROWS = '{d['throws']}' WHERE IDPLAYER = {d['mlbid']}"
-        print(f"{cmd}")
-    else:
-        #print(f"id {d['mlbid']} not found")
-        insertcols = "(IDPLAYER, PLAYERNAME, TEAM, POS, IDFANGRAPHS, FANGRAPHSNAME, ESPNID, ESPNNAME, BATS, THROWS)"
-        idfangraphs = d.get('idfangraphs', 'NULL')
-        fangraphsname = d.get('name', "")
-        espnid = d.get('espnid', "NULL")
-        espnname = d.get('name', "")
-        insertvals = f"({d['mlbid']}, '{d['name']}', '{d['mlbTeam']}', '{d['position']}'," \
-                     f" {idfangraphs}, '{fangraphsname}', {espnid}, '{espnname}', '{d['bats']}', '{d['throws']}')"
-        cmd = f"INSERT INTO IDMAP {insertcols} VALUES {insertvals}"
-        print(f"{cmd}")
+            # print(f"id {d['mlbid']} not found")
+            insertcols = f"(IDPLAYER, PLAYERNAME, TEAM, POS, IDFANGRAPHS, FANGRAPHSNAME, ESPNID, ESPNNAME, MLBID, MLBNAME, BATS, THROWS)"
+            idfangraphs = d.get('idfangraphs', 'NULL')
+            fangraphsname = d.get('name', "")
+            espnid = d.get('espnid', "NULL")
+            espnname = d.get('name', "")
+            insertvals = f"({d['mlbid']}, '{d['name']}', '{d['mlbTeam']}', '{d['position']}'," \
+                         f" {idfangraphs}, '{fangraphsname}', {espnid}, '{espnname}', {d['mlbid']}, '{d['name']}', " \
+                         f"'{d['bats']}', '{d['throws']}')"
+            cmd = f"INSERT INTO IDMAP {insertcols} VALUES {insertvals}"
+            print(f"{cmd}")
 
-    bdb.cmd(cmd)
+        bdb.cmd(cmd)
 
+def main():
+    run_id_map_fixes()
+
+
+if __name__ == "__main__":
+    main()
