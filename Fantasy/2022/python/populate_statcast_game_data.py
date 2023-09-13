84c469748a45afecd0546348f869b693d8db5ed5
Chance <chance_st@yahoo.com>
2023-09-13 10:17:43 -0700

    added refresh module to add team data to StatcastGameData table
    Signed-off-by: Chance <chance_st@yahoo.com>

--- Fantasy/2022/python/populate_statcast_game_data.py
+++ Fantasy/2022/python/populate_statcast_game_data.py
@@ -7,7 +7,7 @@ import sys
 import time
 # noinspection PyCompatibility
 import urllib.request
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 
@@ -109,18 +109,47 @@ def get_start_times(game_date):
     return return_dict
 
 
-def main():
-    #start_date = date(2023, 7, 14)
-    #end_date = date(2023, 10, 3)
-    # If games aren't in the table yet for a given day, populate_one_day does it
-    # It only enters a row for each game ( the game id is known as a gamepk )
-    #[populate_one_day(str(date.fromordinal(i))) for i in range(start_date.toordinal(), end_date.toordinal())]
-    #time.sleep(.5)
-    #update_game_data(716910)
-    #gamedates = get_gamepks_from_db("20230822","20231002")
-    update_games(get_gamepks_from_db("20230822","20230822"))
-    #print(get_start_times("20230822"))
+def refresh_statcast_schedule(statcast_date):
+    table_date = int(statcast_date.replace("-",""))
+    url_name = f"http://statsapi.mlb.com/api/v1/schedule?sportId=1,&date={statcast_date}"
+    print("url is: " + url_name)
+    entries = []
+    column_names = ['date', 'game', 'game_state', 'start_time', 'play_count',
+                    'home_team', 'home_team_id', 'away_team', 'away_team_id']
+    with urllib.request.urlopen(url_name) as url:
+        data = json.loads(url.read().decode())
+        for gamedate in data['dates']:
+            for game in gamedate['games']:
+                # print(game)
+                time.sleep(.5)
+                local_game_time = tools.local_hhmmss_from_mlb_format(game['gameDate'])
+                status = game['status']['statusCode']
+                home_team = game['teams']['home']['team']['name']
+                home_team_id = game['teams']['home']['team']['id']
+                away_team = game['teams']['away']['team']['name']
+                away_team_id = game['teams']['away']['team']['id']
+                entry = [table_date, game['gamePk'], status, local_game_time, None,
+                         home_team, home_team_id, away_team, away_team_id]
+                #print(entry)
+                entries.append(entry)
 
+    df = pd.DataFrame(entries, columns=column_names)
+    print(df)
+    table_name = "StatcastGameData"
+    delcmd = f"delete from {table_name} where date = {table_date}"
+    bdb.delete(delcmd)
+    df.to_sql(table_name, bdb.conn, if_exists='append', index=False)
+
+def main():
+    date1 = '2023-09-18'
+    date2 = '2023-10-03'
+    start_date = datetime.strptime(date1, '%Y-%m-%d')
+    end_date = datetime.strptime(date2, '%Y-%m-%d')
+    step = timedelta(days=1)
+    while start_date <= end_date:
+        print(start_date.strftime("%Y-%m-%d"))
+        refresh_statcast_schedule(start_date.strftime("%Y-%m-%d"))  # %Y-%m-%d
+        start_date += step
 
 
 if __name__ == "__main__":
